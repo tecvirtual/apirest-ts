2b829db8388d975d59555337e481a465f83f1d19
Frank Garcia <garcia1901l@gmail.com>
2023-08-04 12:32:07 -0500

    routes dinamic

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import "dotenv/config"
 import express from "express"
 import cors from "cors"
-import { router } from "./routes/item"
+import { router } from "./routes"
 
 const PORT = process.env.PORT || 3001
 
---
+++ src/routes/index.ts
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import { readdirSync } from "fs";
+
+const PATH_ROUTER = `${__dirname}`
+const router = Router()
+
+/**
+ * 
+ * @returns
+ */
+
+const cleanFileNme = (fileName : String) => {
+    const file = fileName.split('.').shift()
+    return file
+}
+
+readdirSync(PATH_ROUTER).filter((fileName) => {
+    const cleanName = cleanFileNme(fileName)
+    if(cleanName !== 'index'){
+        import(`./${cleanName}`).then((moduleRouter) => {
+            console.log('Cargando las rutas')
+            router.use(`/${cleanName}`, moduleRouter.router)
+        })
+    }
+})
+
+
+
+export { router }
